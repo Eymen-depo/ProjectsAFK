f516916d668b2ccaf2ee7b714b96fc3f73875d71
Eymen-depo <depobulutdepolama@gmail.com>
2026-08-13 11:03:42 +0300

    Change bot username from 'BandirmaVapuru' to 'Witcher_'

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let botConnected = false; // Bot bağlantı durumu
 // Bot yapılandırma ayarları
 const config = {
   botAccount: {
-    username: "BandirmaVapuru",
+    username: "Witcher_",
     password: "fake3",
     type: "legacy"
   },
